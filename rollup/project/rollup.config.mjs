e3aac6655352b73144123c84ad63e2718f200a45
lee <1994166883@qq.com>
2024-06-24 23:12:31 +0800

    rollup config

--- rollup/project/rollup.config.mjs
+++ rollup/project/rollup.config.mjs
@@ -1,10 +1,57 @@
 import { defineConfig } from "rollup";
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+import babel from "@rollup/plugin-babel"
+import commonjs from '@rollup/plugin-commonjs';
 
+/**
+ * @type {import('rollup').RollupOptions;}
+ */
 export default defineConfig({
     input: 'src/index.js',
+    // - output
+    // 1. 单bundle
     output: {
-        file: 'dist/bundle.js',
-        mode: 'umd',
-        name: 'test'
-    }
+        // file: 'dist/bundle.js',
+        dir: 'dist/esm',
+        format: 'esm',
+        entryFileNames: '[name]-[hash:8].js',
+        chunkFileNames: 'chunk-[name]-[hash:8].js',
+        name: 'bundle',
+        // 1. 对象
+        // manualChunks: {
+        //     'lodash-es': ['lodash-es']
+        // },
+        // 2. fun
+        manualChunks: (id) => {
+            if (id.includes('lodash-es')) {
+                return 'lodash-es'
+            }
+        }
+    },
+    // 2.多bundle
+    // output: [
+    //     {
+    //         file: 'dist/iife/bundle.js',
+    //         format: 'iife',
+    //     },
+    //     {
+    //         file: 'dist/amd/bundle.js',
+    //         format: 'amd',
+    //     },
+    //     {
+    //         file: 'dist/esmodule/bundle.js',
+    //         format: 'esm',
+    //     }
+    // ]
+    // external: ['lodash-es'],
+    plugins: [
+        nodeResolve(),
+        commonjs(),
+        babel({
+            babelHelpers: 'runtime',
+            include: 'src/**',
+            exclude: 'node_modules/**',
+            extensions: ['js', 'ts']
+        })
+    ],
 })
--- rollup/project/src/index.js
+++ rollup/project/src/index.js
@@ -1,4 +1,9 @@
 import {getRandom} from './utils.js';
+import {chunk} from 'lodash-es';
+console.log(chunk([1, 2, 3, 4, 5, 6, 7], 2))
+
 let randomNumber = getRandom(0, 10)
+
+Promise.resolve(3).then(a => console.log(a))
 
 console.log(randomNumber)
