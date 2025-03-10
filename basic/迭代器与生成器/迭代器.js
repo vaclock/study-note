7b1136b5d78b56f55e86d3b29e844ca95253a16c
ownclock <lee.ownclock@gmail.com>
2025-03-10 19:53:46 +0800

    迭代器

--- basic/迭代器与生成器/迭代器.js
+++ basic/迭代器与生成器/迭代器.js
@@ -59,6 +59,7 @@ function* fibonacci() {
   }
 }
 
+
 const fib = fibonacci()
 console.log(fib.next().value)
 console.log(fib.next().value)
@@ -66,3 +67,54 @@ console.log(fib.next().value)
 console.log(fib.next().value)
 console.log(fib.next().value)
 
+
+// 使用生成器实现一个简单的异步任务队列
+const asyncTaskQueue = [
+  (param) => new Promise((resolve) => {
+    setTimeout(() => {
+      console.log(`任务 1 结束 ✅，参数: ${param}`);
+      resolve(`来自任务1的参数`);
+    }, 1000);
+  }),
+  (param) => new Promise((resolve) => {
+    setTimeout(() => {
+      console.log(`任务 2 结束 ✅，参数: ${param}`);
+      resolve(`来自任务2的参数`);
+    }, 1100);
+  }),
+  (param) => new Promise((resolve) => {
+    setTimeout(() => {
+      console.log(`任务 3 结束 ✅，参数: ${param}`);
+      resolve(`来自任务3的参数`);
+    }, 1200);
+  }),
+  (param) => new Promise((resolve) => {
+    setTimeout(() => {
+      console.log(`任务 4 结束 ✅，参数: ${param}`);
+      resolve(`来自任务4的参数`);
+    }, 1300);
+  })
+];
+
+function* asyncTaskGenerator(asyncTaskQueue, initialParam) {
+  let param = initialParam;
+  for (const task of asyncTaskQueue) {
+    param = yield task(param);
+  }
+}
+
+function runTask(asyncTaskGenerator, initialParam) {
+  const iterator = asyncTaskGenerator(asyncTaskQueue, initialParam);
+  console.log(iterator, 'iterator==')
+  function run(result) {
+    if (result.done) return; // 任务全部完成
+
+    result.value.then(res => {
+      run(iterator.next(res)); // 继续执行下一个任务，并传递上一个任务的结果
+    });
+  }
+
+  run(iterator.next()); // 启动任务队列
+}
+
+runTask(asyncTaskGenerator, '初始参数')
